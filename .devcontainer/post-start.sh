78612cb3fe18a5ee9eb0191ba128359c60712405
Vasil Ivanov <59696861+vasilvas99@users.noreply.github.com>
2022-11-23 13:34:27 +0000

    move installations to dockerfile

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -21,26 +21,17 @@ sudo ip link add dev can0 type vcan || true
 sudo ip link set can0 up || true
 
 # setup openssh server service
-sudo apt-get update -y
-sudo apt-get install -y openssh-server
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
 # setup k3s and kubectl
-sudo wget "https://github.com/k3s-io/k3s/releases/download/v1.22.16%2Bk3s1/k3s" -P /usr/local/bin/
-sudo chmod +x /usr/local/bin/k3s
-sudo cp $CODESPACE_VSCODE_FOLDER/.devcontainer/resources/kubectl /usr/local/bin/
-sudo curl "https://raw.githubusercontent.com/k3s-io/k3s/master/k3s.service" -o /etc/systemd/system/k3s.service
 sudo systemctl enable k3s
 sudo systemctl start k3s
 
 # add containerd service
-sudo cp $CODESPACE_VSCODE_FOLDER/.devcontainer/resources/containerd.service /etc/systemd/system
 sudo systemctl enable containerd
 sudo systemctl start containerd
 
 # add mosquitto service
-sudo apt-get install -y mosquitto
-sudo cp $CODESPACE_VSCODE_FOLDER/.devcontainer/resources/mosquitto.service /etc/systemd/system
 sudo systemctl enable mosquitto
 sudo systemctl start mosquitto
