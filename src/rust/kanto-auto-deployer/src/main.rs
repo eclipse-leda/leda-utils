2a1ef69b53cbc75651520ee45cf84b5e7d42b698
Stoyan Lachev <stoyan.lachev@bosch.io>
2022-12-05 14:25:03 +0000

    Change Tabs to Spaces
    Signed-off-by: Stoyan Lachev <stoyan.lachev@bosch.io>

--- src/rust/kanto-auto-deployer/src/main.rs
+++ src/rust/kanto-auto-deployer/src/main.rs
@@ -40,29 +40,29 @@ async fn create(_client: &mut kanto::containers_client::ContainersClient<tonic::
     let container_str = fs::read_to_string(file_path)?;
     let parsed_json = serde_json::from_str(&container_str);
 	if let Ok(container) = parsed_json {
-		let container: kanto_cnt::Container = container;
-		let name = String::from(container.name.clone());
-		let _r = tonic::Request::new(kanto::ListContainersRequest {});
-		let containers = _client.list(_r).await?.into_inner();
-		for cont in &containers.containers {
-			if cont.name == name {
-				println!("Already exists [{}]", name);
-				return Ok(());    		  
-			}
-		}
-		println!("Creating [{}]", name);
-		let request = tonic::Request::new(kanto::CreateContainerRequest{container: Some(container)});
-		let _response = _client.create(request).await?;
-		println!("Created [{}]", name);
-		let _none = String::new();
-		let id = match _response.into_inner().container {
-			Some(c) => c.id,
-			None => _none
-		};
-		start(_client, &name, &id).await?;
-	} else {
-		eprintln!("Wrong json in [{}]", file_path);
-	}
+        let container: kanto_cnt::Container = container   
+        let name = String::from(container.name.clone());
+        let _r = tonic::Request::new(kanto::ListContainersRequest {});
+        let containers = _client.list(_r).await?.into_inner();
+        for cont in &containers.containers {
+            if cont.name == name {
+                println!("Already exists [{}]", name);
+                return Ok(());    		  
+            }
+        }
+        println!("Creating [{}]", name);
+        let request = tonic::Request::new(kanto::CreateContainerRequest{container: Some(container)});
+        let _response = _client.create(request).await?;
+        println!("Created [{}]", name);
+        let _none = String::new();
+        let id = match _response.into_inner().container {
+            Some(c) => c.id,
+            None => _none
+        };
+        start(_client, &name, &id).await?;
+    } else {
+        eprintln!("Wrong json in [{}]", file_path);
+    }
     Ok(())	
 }
 
