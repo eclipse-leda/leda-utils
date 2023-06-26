5d8e38bd8a5353fd1903ab26a2bed0561fe7d305
vasilvas99 <vasil.ivanov3@bosch.io>
2023-06-26 16:56:49 +0300

    add lock handling

--- src/rust/kanto-auto-deployer/src/mqtt_listener.rs
+++ src/rust/kanto-auto-deployer/src/mqtt_listener.rs
@@ -3,16 +3,56 @@ use anyhow::{anyhow, Result};
 use rumqttc::{self, Client, Event::Incoming, MqttOptions, Packet::Publish, QoS};
 use serde_json::{self, Value};
 use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
 use std::time::Duration;
 
-static SERVICE_ID: &str = "kanto-auto-deployer";
+static SERVICE_ID: &str = "kanto_auto_deployer";
+static LOCK_NAME: &str = "KAD.enabled";
 static TERMINATE_KEY_JSON: &str = "terminate";
 static RECONNECT_TIMEOUT: u64 = 2;
 
-fn handle_mqtt_payload(payload: &[u8], thread_terminate_flag: &AtomicBool) -> Result<()> {
+fn obtain_lock_path(cli_config: &CliArgs) -> Result<PathBuf> {
+    let manifests_dir = fs::canonicalize(cli_config.manifests_path.clone())?;
+    let lock_full = manifests_dir.join(LOCK_NAME);
+    if lock_full.is_file() {
+        return Ok(lock_full);
+    } else {
+        return Err(anyhow!(
+            "Path {:?} does not exist or is not a file!",
+            lock_full
+        ));
+    }
+}
+
+fn disable_kad(lock_path: &PathBuf) -> Result<()> {
+    let mut disabled_lock_path = lock_path.clone();
+
+    if !lock_path.is_file() {
+        return Err(anyhow!(
+            "{:?} is not a regular file or does not exist",
+            lock_path
+        ));
+    }
+    if !disabled_lock_path.set_extension("disabled") {
+        return Err(anyhow!(
+            "Could not change extension of {:?} to *.disabled",
+            lock_path
+        ));
+    }
+    fs::rename(lock_path, disabled_lock_path)?;
+
+    Ok(())
+}
+
+fn handle_mqtt_payload(
+    payload: &[u8],
+    lock_path: &PathBuf,
+    thread_terminate_flag: &AtomicBool,
+) -> Result<()> {
     // We don't care about non-json messages
     let terminate_flag_mqtt = serde_json::from_slice::<HashMap<String, Value>>(payload)?
         .get(TERMINATE_KEY_JSON)
@@ -24,6 +64,9 @@ fn handle_mqtt_payload(payload: &[u8], thread_terminate_flag: &AtomicBool) -> Re
 
     // Only if an actual termination request is received, update the atomic flag
     if terminate_flag_mqtt {
+        if let Err(e) = disable_kad(&lock_path) {
+            log::error!("Could not set KAD lock to disabled: {e}")
+        }
         thread_terminate_flag.store(true, Ordering::Relaxed);
     }
 
@@ -50,7 +93,19 @@ pub fn mqtt_main(cli_config: Arc<CliArgs>, thread_terminate_flag: &AtomicBool) -
         "Trying to start MQTT connection with options {:?}",
         &cli_config.mqtt
     );
-    
+
+    let lock_path;
+    match obtain_lock_path(&cli_config) {
+        Ok(lock) => {
+            lock_path = lock;
+        }
+        Err(e) => {
+            log::error!("Could not obtain {LOCK_NAME}. Will continue running in daemon mode \
+            but will not start the MQTT listener. Err: {e}");
+            return Ok(());
+        }
+    }
+
     let mut mqttoptions = MqttOptions::new(SERVICE_ID, &cli_config.mqtt.ip, cli_config.mqtt.port);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
     let delta = Duration::from_secs(RECONNECT_TIMEOUT);
@@ -63,7 +118,7 @@ pub fn mqtt_main(cli_config: Arc<CliArgs>, thread_terminate_flag: &AtomicBool) -
         // We only care about incoming messages
         if let Ok(msg) = notification {
             if let Incoming(Publish(pub_msg)) = msg {
-                let _r = handle_mqtt_payload(&pub_msg.payload, thread_terminate_flag);
+                let _r = handle_mqtt_payload(&pub_msg.payload, &lock_path, thread_terminate_flag);
                 if let Err(e) = _r {
                     log::debug!("MQTT message parsing error: {e}")
                 }
