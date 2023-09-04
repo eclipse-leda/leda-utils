fb1db14cbab4ba90dc9f352269395cbf4b799bbb
Vasil Ivanov <vasil.ivanov3@bosch.io>
2023-09-04 17:24:23 +0300

    initial commit for blueprints selector

---
+++ src/rust/blueprints-selector/README.md
@@ -0,0 +1,73 @@
+# Blueprint Selector
+
+A Rust application that reads all blueprint files in a directory and provides the user with an
+interactive (supports filtering) menu to select the one to be deployed on the edge device via MQTT
+
+![Screenshot](./images/blueprint-selector-screenshot.png)
+
+The "blueprint" files are desired state messages, that have the extension ".blueprint.json" (configurable) and an additional metadata header (required):
+```json
+{
+  "blueprintMetadata": {
+    "name": "<NAME>",
+    "description": "<Short Description>"
+  },
+  "activityId": "correlation-id",
+  "timestamp": 1235466
+  "payload": {} // This is the desired state message
+}
+```
+The metadata header will be used to construct the list of options presented to the user. You can find example blueprints under the
+`example_blueprints` directory in this repository.
+
+The selected blueprint is then published as an MQTT message on the `vehicleupdate/desiredstate` topic.
+
+# CLI options
+
+To get all available MQTT use the `--help/-h` flag:
+
+```shell
+$ blueprint-selector --help
+A user-friendly tool to select and deploy SDV blueprints
+
+Usage: blueprint-selector [OPTIONS]
+
+Options:
+  -d, --blueprints-dir <BLUEPRINTS_DIR>
+          The directory containing the SDV bluerprints [default: /var/containers/blueprints]
+  -e, --blueprints-ext <BLUEPRINT_EXTENSION>
+          Extension to use when iterating over the files in the blueprints directory [default: .blueprint.json]
+      --mqtt-broker-host <HOST>
+          Hostname/IP to the MQTT broker where the desired state message would be posted [default: 127.0.0.1]
+      --mqtt-broker-port <PORT>
+          Port for the MQTT broker [default: 1883]
+      --mqtt-topic <TOPIC>
+          Topic on which to publish the blueprint desired state message [default: vehicleupdate/desiredstate]
+  -h, --help
+          Print help
+  -V, --version
+          Print version
+```
+
+# Other notes
+
+1. You can set the `DEFAULT_BLUEPRINTS_DIR` environment variable during compilation to change the default blueprints dir
+(allows the user to call the blueprint-selector with no argument when the defaults are good).
+
+2. Uses runmqttc as a MQTT (4 only) client, so the binary is completely statically linked (i.e. does not require the user to have paho mqtt)
+
+3. The mqtt-broker-host can be any valid hostname.
+
+# Compiling
+
+To compile the binary in release mode, obtain the [Rust toolchain](https://www.rust-lang.org/tools/install) (version 1.68.2+) and run:
+
+```shell
+    $ cargo build --release
+```
+
+If you decide to change the default blueprints directory, run:
+
+```shell
+    $ DEFAULT_BLUEPRINTS_DIR="/new/default/path" cargo build --release
+```
---
+++ src/rust/blueprints-selector/src/main.rs
@@ -0,0 +1,163 @@
+// ********************************************************************************
+// * Copyright (c) 2023 Contributors to the Eclipse Foundation
+// *
+// * See the NOTICE file(s) distributed with this work for additional
+// * information regarding copyright ownership.
+// *
+// * This program and the accompanying materials are made available under the
+// * terms of the Apache License 2.0 which is available at
+// * https://www.apache.org/licenses/LICENSE-2.0
+// *
+// * SPDX-License-Identifier: Apache-2.0
+// ********************************************************************************
+
+use anyhow::{anyhow, Result};
+use clap::{Args, Parser};
+use inquire::Select;
+use lazy_static::lazy_static;
+use rumqttc::{mqttbytes::v4::Packet, Client, Event, MqttOptions, QoS};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Map, Value};
+use std::path::{Path, PathBuf};
+use std::time::Duration;
+use std::{fmt::Display, fs::File};
+use walkdir::WalkDir;
+
+// Makes it easier when building through BitBake recipes to change the default look-up location and enable
+// calling with zero arguments from the end user.
+// On an Leda Image this would be set to point at the persistent data partition, e.g.
+// DEFAULT_BLUEPRINTS_DIR="/data/var/containers/blueprints" in the recipe itself
+lazy_static! {
+    static ref DEFAULT_BLUEPRINTS_DIR: &'static str = {
+        match std::option_env!("DEFAULT_BLUEPRINTS_DIR") {
+            Some(p) => p,
+            None => "/var/containers/blueprints",
+        }
+    };
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BlueprintMetadata {
+    name: String,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Blueprint {
+    #[serde(alias = "blueprintMetadata")]
+    metadata: BlueprintMetadata,
+    #[serde(alias = "activityId")]
+    activity_id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    timestamp: Option<u64>,
+    payload: Map<String, Value>,
+}
+
+impl Display for Blueprint {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} ({})", self.metadata.name, self.metadata.description)
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct MQTTconfig {
+    /// Hostname/IP to the MQTT broker where the desired state message would be posted
+    #[arg(long = "mqtt-broker-host", default_value = "127.0.0.1")]
+    host: String,
+
+    /// Port for the MQTT broker
+    #[arg(long = "mqtt-broker-port", default_value_t = 1883)]
+    port: u16,
+
+    /// Topic on which to publish the blueprint desired state message
+    #[arg(long = "mqtt-topic", default_value = "vehicleupdate/desiredstate")]
+    topic: String,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct CLIargs {
+    /// The directory containing the SDV bluerprints
+    #[arg(short = 'd', long = "blueprints-dir", default_value=*DEFAULT_BLUEPRINTS_DIR)]
+    blueprints_dir: PathBuf,
+
+    /// Extension to use when iterating over the files in the blueprints directory
+    #[arg(
+        short = 'e',
+        long = "blueprints-ext",
+        default_value = ".blueprint.json"
+    )]
+    blueprint_extension: String,
+
+    #[clap(flatten)]
+    mqtt: MQTTconfig,
+}
+
+fn parse_blueprint(blueprint_path: &Path) -> Result<Blueprint> {
+    let f = File::open(blueprint_path)?;
+    match serde_json::from_reader(f) {
+        Ok(blueprint) => Ok(blueprint),
+        Err(e) => Err(anyhow!("{}", e)),
+    }
+}
+
+fn has_extension(p: &walkdir::DirEntry, extension: &str) -> bool {
+    p.path().to_str().unwrap().ends_with(extension)
+}
+
+fn load_blueprints(dir: &Path, extension: &str) -> Vec<Blueprint> {
+    WalkDir::new(dir)
+        .min_depth(1)
+        .into_iter()
+        .filter_entry(|p| has_extension(p, extension))
+        .filter_map(Result::ok)
+        .map(|blueprint_path| parse_blueprint(blueprint_path.path()))
+        .filter_map(Result::ok)
+        .collect()
+}
+
+fn publish_blueprint(message: &str, mqtt_conf: &MQTTconfig) -> Result<()> {
+    let mut mqttoptions = MqttOptions::new("blueprints-selector", &mqtt_conf.host, mqtt_conf.port);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    client.publish(&mqtt_conf.topic, QoS::ExactlyOnce, false, message)?;
+
+    // Spin the event loop and wait for packet acknowledment
+    for (_i, notification) in connection.iter().enumerate() {
+        if let Event::Incoming(Packet::PubComp(_msg)) = notification? {
+            println!("Succesfully published blueprint.");
+            return Ok(());
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cli = CLIargs::parse();
+    let blueprint_options = load_blueprints(&cli.blueprints_dir, &cli.blueprint_extension);
+
+    let selection_result = Select::new(
+        "Choose a SDV blueprint which you would like to deploy:",
+        blueprint_options,
+    )
+    .prompt();
+
+    let blueprint = match selection_result {
+        Ok(blueprint) => blueprint,
+        Err(e) => {
+            println!("Exiting abruptly: {}.", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!(
+        "Publishing blueprint \"{}\" on topic \"{}\".",
+        blueprint.metadata.name, cli.mqtt.topic
+    );
+    let message = serde_json::to_string(&blueprint)?;
+
+    publish_blueprint(&message, &cli.mqtt)?;
+
+    Ok(())
+}
