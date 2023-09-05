282830151f345eb80b7eba0c76ccfedadd6d1b39
Vasil Ivanov <vasil.ivanov3@bosch.io>
2023-09-05 15:01:01 +0300

    implement git https fetcher

--- src/rust/blueprint-selector/src/blueprint_fetchers.rs
+++ src/rust/blueprint-selector/src/blueprint_fetchers.rs
@@ -1,11 +1,14 @@
 use std::{fmt::Display, path::Path, rc::Rc};
 use anyhow::{anyhow, Result};
 use strum::{IntoEnumIterator, EnumIter};
-use reqwest::{Url, blocking};
+use reqwest::{Url, blocking,};
+use tempdir::TempDir;
+use git2::Repository;
 
 #[derive(Debug, EnumIter)]
 pub enum FetcherKind {
     HttpSingleFile,
+    Git
 }
 
 pub struct Fetcher {
@@ -18,8 +21,23 @@ impl Display for FetcherKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match  self {
             FetcherKind::HttpSingleFile => write!(f, "Single Blueprint File Over HTTP"),
+            FetcherKind::Git => write!(f, "Clone a Git repository over HTTP(S)")
         }
     }
+}
+
+pub fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> Result<()> {
+    std::fs::create_dir_all(&destination)?;
+    for entry in std::fs::read_dir(source)? {
+        let entry = entry?;
+        let filetype = entry.file_type()?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        } else {
+            std::fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
 
 impl FetcherKind {
@@ -30,7 +48,7 @@ impl FetcherKind {
 
 impl Fetcher {
     pub fn new(kind: FetcherKind, uri: &str, output_dir: &Path)-> Result<Self> {
-        let output_dir = Rc::from(output_dir);
+        let output_dir = Rc::from(std::fs::canonicalize(output_dir)?);
         let uri = Url::parse(uri)?;
         Ok(Fetcher { kind, uri, output_dir})
     }
@@ -44,10 +62,20 @@ impl Fetcher {
         Ok(())
     }
 
+    fn git_repo(self) -> Result<()> {
+        let temp_dir = TempDir::new("blueprints_repo")?;
+        println!("Cloning into repository.");
+        let _repo = Repository::clone(self.uri.as_str(), &temp_dir)?;
+        copy_recursively(&temp_dir, self.output_dir)?;
+        // temp_dir is a RAII object and is deleted on instance Drop
+        Ok(())
+    }
+
     // move out of self, preventing multiple use of he same fetcher
     pub fn fetch(self) -> Result<()> {
         match self.kind {
-            FetcherKind::HttpSingleFile => self.http_single_file()?
+            FetcherKind::HttpSingleFile => self.http_single_file()?,
+            FetcherKind::Git => self.git_repo()?,
         }
 
         Ok(())
